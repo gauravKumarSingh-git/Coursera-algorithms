4407259697df6dae0be02193d49f4c7a3b079b1f
Gaurav Kumar Singh <gauravkumarsingh@Gauravs-MacBook-Air.local>
2024-01-02 00:22:13 +0530

    percolation updated

--- Assignment-week1/src/percolation/Percolation.java
+++ Assignment-week1/src/percolation/Percolation.java
@@ -6,75 +6,78 @@ public class Percolation {
 
     private boolean[][] arr;
     private int n;
-    public int openSites = 0;
-    public WeightedQuickUnionUF wquickFind;
+    private int openSites = 0;
+    private WeightedQuickUnionUF wquickFind;
 
     // creates n-by-n grid, with all sites initially blocked
-    public Percolation(int n){
+    public Percolation(int n) {
+        if(n <= 0) throw new IllegalArgumentException();
         this.n = n;
-        this.wquickFind = new WeightedQuickUnionUF(n*n + 2);
-        for(int i = 1; i <= n ; i++){
+        this.wquickFind = new WeightedQuickUnionUF(n * n + 2);
+        for (int i = 1; i <= n; i++) {
             wquickFind.union(0, i);
         }
-        for(int i = n*(n-1) + 1; i <= n*n; i++){
-            wquickFind.union(n*n + 1, i);
+        for (int i = n * (n - 1) + 1; i <= n * n; i++) {
+            wquickFind.union(n * n + 1, i);
         }
         arr = new boolean[n][n];
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 arr[i][j] = false;
             }
         }
     }
 
     // opens the site (row, col) if it is not open already
-    public void open(int row, int col) throws IllegalArgumentException{
-        if(row <= 0 || row > n || col <= 0 || col > n) throw new IllegalArgumentException();
-        if(isFull(row, col)){
-            arr[row-1][col-1] = true;
+    public void open(int row, int col) {
+        if (row <= 0 || row > n || col <= 0 || col > n) throw new IllegalArgumentException();
+        if (!isOpen(row, col)) {
+            arr[row - 1][col - 1] = true;
             openSites++;
 
             // write connection logic with neighbouring open sites
-            if(row-2 >= 0 && isOpen(row-1, col)){
-                wquickFind.union((row-1)*n + col, (row-2)*n + col);
+            if (row - 2 >= 0 && isOpen(row - 1, col)) {
+                wquickFind.union((row - 1) * n + col, (row - 2) * n + col);
             }
-            if(row < n && isOpen(row+1, col)){
-                wquickFind.union((row-1)*n + col, (row)*n + col);
+            if (row < n && isOpen(row + 1, col)) {
+                wquickFind.union((row - 1) * n + col, (row) * n + col);
             }
-            if(col-2 >= 0 && isOpen(row, col-1)){
-                wquickFind.union((row-1)*n + col, (row-1)*n + col-1);
+            if (col - 2 >= 0 && isOpen(row, col - 1)) {
+                wquickFind.union((row - 1) * n + col, (row - 1) * n + col - 1);
             }
-            if(col < n && isOpen(row, col+1)){
-                wquickFind.union((row-1)*n + col, (row-1)*n + col+1);
+            if (col < n && isOpen(row, col + 1)) {
+                wquickFind.union((row - 1) * n + col, (row - 1) * n + col + 1);
             }
         }
 
     }
 
     // is the site (row, col) open?
-    public boolean isOpen(int row, int col) throws IllegalArgumentException{
-        if(row <= 0 || row > n || col <= 0 || col > n) throw new IllegalArgumentException();
-        return arr[row-1][col-1];
+    public boolean isOpen(int row, int col) {
+        if (row <= 0 || row > n || col <= 0 || col > n) throw new IllegalArgumentException();
+        return arr[row - 1][col - 1];
     }
 
     // is the site (row, col) full?
-    public boolean isFull(int row, int col) throws IllegalArgumentException{
-        if(row <= 0 || row > n || col <= 0 || col > n) throw new IllegalArgumentException();
-        return !arr[row-1][col-1];
+    public boolean isFull(int row, int col) {
+        if (row <= 0 || row > n || col <= 0 || col > n) throw new IllegalArgumentException();
+        return isOpen(row, col)  && wquickFind.find(0) == wquickFind.find((row-1)*n + col);
+//        return !arr[row - 1][col - 1];
     }
 
     // returns the number of open sites
-    public int numberOfOpenSites(){
+    public int numberOfOpenSites() {
         return openSites;
     }
 
     // does the system percolate?
-    public boolean percolates(){
-        return wquickFind.find(0) == wquickFind.find(n*n+1);
+    public boolean percolates() {
+        if(numberOfOpenSites() == 0) return false;
+        return wquickFind.find(0) == wquickFind.find(n * n + 1);
     }
 
     // test client (optional)
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
     }
 }
--- Assignment-week1/src/percolation/PercolationStats.java
+++ Assignment-week1/src/percolation/PercolationStats.java
@@ -4,52 +4,51 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-    Percolation percolation;
-    double[] results;
-    int T;
+    private Percolation percolation;
+    private double[] results;
+    private int T;
 
-    public PercolationStats(){}
     // perform independent trials on an n-by-n grid
-    public PercolationStats(int n, int trials){
-        if(n <= 0 || trials <= 0) throw new IllegalArgumentException();
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) throw new IllegalArgumentException();
         this.T = trials;
         results = new double[trials];
-        for(int i = 0; i < trials; i++){
+        for (int i = 0; i < trials; i++) {
             percolation = new Percolation(n);
-            while(!percolation.percolates()){
-                int j = (int)StdRandom.uniformDouble(1, n + 1);
-                int k = (int)StdRandom.uniformDouble(1, n + 1);
+            while (!percolation.percolates()) {
+                int j = (int) StdRandom.uniformDouble(1, n + 1);
+                int k = (int) StdRandom.uniformDouble(1, n + 1);
                 percolation.open(j, k);
             }
-            results[i] = (percolation.numberOfOpenSites() * 1.0)/(n*n);
+            results[i] = (percolation.numberOfOpenSites() * 1.0) / (n * n);
         }
     }
 
     // sample mean of percolation threshold
-    public double mean(){
+    public double mean() {
         return StdStats.mean(results);
     }
 
     // sample standard deviation of percolation threshold
-    public double stddev(){
+    public double stddev() {
         return StdStats.stddev(results);
     }
 
     // low endpoint of 95% confidence interval
-    public double confidenceLo(){
-        return mean() - (1.96*stddev())/Math.sqrt(T);
+    public double confidenceLo() {
+        return mean() - (1.96 * stddev()) / Math.sqrt(T);
     }
 
     // high endpoint of 95% confidence interval
-    public double confidenceHi(){
-        return mean() + (1.96*stddev())/Math.sqrt(T);
+    public double confidenceHi() {
+        return mean() + (1.96 * stddev()) / Math.sqrt(T);
     }
 
     // test client (see below)
-    public static void main(String[] args){
-        int n = Integer.parseInt(args[0]), T = Integer.parseInt(args[1]);
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]), t = Integer.parseInt(args[1]);
 //        Stopwatch stopwatch = new Stopwatch();
-        PercolationStats stats = new PercolationStats(n, T);
+        PercolationStats stats = new PercolationStats(n, t);
         System.out.println("mean\t\t\t\t\t= " + stats.mean());
         System.out.println("stddev\t\t\t\t\t= " + stats.stddev());
         System.out.println("95% confidence interval = [" + stats.confidenceLo() + ", " + stats.confidenceHi() + "]");
