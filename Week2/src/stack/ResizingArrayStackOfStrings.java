a1540ae022fa074a0e8fe567a4835d3b6a89b743
Gaurav Kumar Singh <gauravkumarsingh@Gauravs-MacBook-Air.local>
2024-01-14 02:07:16 +0530

    Resizing array Stack Of Strings

--- Week2/src/stack/ResizingArrayStackOfStrings.java
+++ Week2/src/stack/ResizingArrayStackOfStrings.java
@@ -2,16 +2,19 @@ package stack;
 
 import java.util.Scanner;
 
-public class StackOfStringsArray {
+public class ResizingArrayStackOfStrings {
 
     private String[] stack = null;
     private int n = 0;
 
-    public StackOfStringsArray(int len) {
-        stack = new String[len];
+    public ResizingArrayStackOfStrings(){
+        stack = new String[1];
     }
 
     public void push(String item){
+        if(n == stack.length){
+            resize(n*2);
+        }
         stack[n++] = item;
     }
 
@@ -19,6 +22,7 @@ public class StackOfStringsArray {
         if(isEmpty()) throw new ArrayIndexOutOfBoundsException("Empty Stack");
         String item = stack[--n];
         stack[n] = null;
+        if(n > 0 && n == stack.length/4){ resize(stack.length/2); }
         return item;
     }
 
@@ -26,14 +30,27 @@ public class StackOfStringsArray {
         return n == 0;
     }
 
+    private void resize(int capacity){
+        String[] newStack = new String[capacity];
+        for(int i = 0; i < n; i++){
+            newStack[i] = stack[i];
+        }
+        this.stack = newStack;
+    }
+
+    public String stackLength(){
+        return "capacity : " + stack.length + " size : " + n;
+    }
+
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        StackOfStringsArray stack = new StackOfStringsArray(10);
+        ResizingArrayStackOfStrings stack = new ResizingArrayStackOfStrings();
         while(true){
             System.out.println("Enter choice");
             System.out.println("1. push");
             System.out.println("2. pop");
-            System.out.println("3. isEmpty");
+            System.out.println("3. capacity");
+            System.out.println("4. isEmpty");
 
             int choice = scan.nextInt();
             if(choice == 1){
@@ -43,6 +60,8 @@ public class StackOfStringsArray {
             }else if(choice == 2){
                 System.out.println(stack.pop());
             }else if(choice == 3){
+                System.out.println(stack.stackLength());
+            }else if(choice == 4){
                 System.out.println("Empty ? : " + stack.isEmpty());
             }else {
                 break;
