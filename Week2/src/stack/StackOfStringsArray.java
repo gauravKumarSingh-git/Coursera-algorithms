712e0f27dabd3f0776bbde0ab5aea0f63563f608
Gaurav Kumar Singh <gauravkumarsingh@Gauravs-MacBook-Air.local>
2024-01-13 23:52:23 +0530

    stack by array

---
+++ Week2/src/stack/StackOfStringsArray.java
@@ -0,0 +1,23 @@
+package stack;
+
+public class StackOfStringsArray {
+
+    private String[] stack = null;
+    private int first = -1;
+
+    public StackOfStringsArray(int n) {
+        stack = new String[n];
+    }
+
+    public void push(String item){
+
+    }
+
+    public String pop(){
+
+    }
+
+    public boolean isEmpty(){
+        return true;
+    }
+}
