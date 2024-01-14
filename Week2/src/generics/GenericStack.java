d92281292332c306076bfb71592deb09dcef4592
gauravKumarSingh-git <gauravkumarsingh.be18ele@pec.edu.in>
2024-01-14 16:46:36 +0530

    implemented Iterable in generic stack

--- Week2/src/generics/GenericStack.java
+++ Week2/src/generics/GenericStack.java
@@ -2,17 +2,38 @@ package generics;
 
 import stack.ResizingArrayStackOfStrings;
 
+import java.util.Iterator;
 import java.util.Scanner;
 
 /**
  * Generic array creation is not allowed in java, so have to do type casting
  */
-public class GenericStack<Item> {
+public class GenericStack<Item> implements Iterable<Item>{
     private Item[] stack = null;
     private int n = 0;
 
     public GenericStack(){
         stack = (Item[]) new Object[1];
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new ArrayIterator();
+    }
+
+    private class ArrayIterator implements Iterator<Item>{
+
+        private int i = n;
+
+        @Override
+        public boolean hasNext() {
+            return i > 0;
+        }
+
+        @Override
+        public Item next() {
+            return stack[--i];
+        }
     }
 
     public void push(Item item){
@@ -46,6 +67,15 @@ public class GenericStack<Item> {
         return "capacity : " + stack.length + " size : " + n;
     }
 
+    public String toString(){
+        String ret = "[";
+        for(Item i : stack){
+            ret += i + ", ";
+        }
+        ret += "]";
+        return ret;
+    }
+
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         GenericStack<Integer> stack = new GenericStack<>();
@@ -55,6 +85,7 @@ public class GenericStack<Item> {
             System.out.println("2. pop");
             System.out.println("3. capacity");
             System.out.println("4. isEmpty");
+            System.out.println("5. Print stack");
 
             int choice = scan.nextInt();
             if(choice == 1){
@@ -67,9 +98,13 @@ public class GenericStack<Item> {
                 System.out.println(stack.stackLength());
             }else if(choice == 4){
                 System.out.println("Empty ? : " + stack.isEmpty());
+            }else if(choice == 5){
+                System.out.println(stack.toString());
             }else {
                 break;
             }
         }
     }
+
+
 }
